d8233cc181970698b27e5427acc6e8e6d0dfbe3e
chuckzzzz <39171423+chuckzzzz@users.noreply.github.com>
2021-05-10 17:43:24 -0700

    Update split_vcf.py
    fixed indentations

--- split_vcf.py
+++ split_vcf.py
@@ -3,7 +3,7 @@
 # Input: a vcf file, gzipped or non-zipped  
 #        an output directory to store the files 
 # Output: Output separate files for each of the chromosomes into specified directory 
-# Example: python split.py -f /storage/czhang/projects/STR_expression/data/hg38.SNP.gz -o /storage/czhang/projects/STR_expression/data/hg38_SNP_genotypes/ -index -h
+# Example: python split.py -f /storage/czhang/projects/STR_expression/data/hg38.SNP.gz -o /storage/czhang/projects/STR_expression/data/hg38_SNP_genotypes/ -index -header
 
 import argparse
 import subprocess
@@ -14,7 +14,7 @@ parser = argparse.ArgumentParser(description='Split vcf by chromosome and build 
 parser.add_argument('-f', type=str, dest="f", help='input vcf file')
 parser.add_argument('-o', type=str, default="./", dest="o", help='output directory')
 parser.add_argument("-index", action="store_true",help='whether to build index')
-parser.add_argument('-h', dest='header', default=False, action='store_true')
+parser.add_argument('-header', dest='header', default=False, action='store_true')
 
 args = parser.parse_args()
 
@@ -32,27 +32,27 @@ if __name__=="__main__":
     store_header=args.header
 
     print("INFO: input file: {}\nINFO: output directory: {}\nINFO: build index {}\nINFO: include header {}".format(vcf_file,output_dir,str(build_index),str(store_header)))
-	
+    
     if is_gz_file(vcf_file)==False:
         print("INFO: vcf file not gzipped, gzipping")
         subprocess.check_call(["bgzip","-c",vcf_file,">",vcf_file+".gz"], shell=True)
         vcf_file=vcf_file+".gz"
     vcf_dir=os.path.dirname(vcf_file)
     if os.path.isfile(vcf_file+".tbi")==False:
-	    print("INFO: building vcf index...")
-	    subprocess.check_call(["tabix","-p","vcf",vcf_file])
-	
+        print("INFO: building vcf index...")
+        subprocess.check_call(["tabix","-p","vcf",vcf_file])
+    
     for i in tqdm(range(1,23)):
-	    cur_chrom="chr"+str(i)
-	    cur_output=output_dir+cur_chrom
-	    try:    	
-	    	with open(cur_output,'wb') as f:
-			if(store_header):
-				subprocess.check_call(["tabix","-h",vcf_file,cur_chrom], stdout=f)
-			else:
-	    			subprocess.check_call(["tabix",vcf_file,cur_chrom], stdout=f)
-	    except subprocess.CalledProcessError as e:
-	    	raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
-	    subprocess.check_call(["bgzip",cur_output])
-	    if build_index:
-	        subprocess.check_call(["tabix","-p","vcf",cur_output+".gz"])
+        cur_chrom="chr"+str(i)
+        cur_output=output_dir+cur_chrom
+        try:
+            with open(cur_output,'wb') as f:
+                if(store_header):
+                    subprocess.check_call(["tabix","-h",vcf_file,cur_chrom], stdout=f)
+                else:
+                    subprocess.check_call(["tabix",vcf_file,cur_chrom], stdout=f)
+        except subprocess.CalledProcessError as e:
+            raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+        subprocess.check_call(["bgzip",cur_output])
+        if build_index:
+            subprocess.check_call(["tabix","-p","vcf",cur_output+".gz"])    
