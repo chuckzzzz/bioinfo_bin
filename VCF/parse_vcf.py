c8010fa1132bcfa2f87922fe6198180d1898e0ef
chuckzzzz <39171423+chuckzzzz@users.noreply.github.com>
2021-10-04 01:25:27 -0500

    Add files via upload

---
+++ VCF/parse_vcf.py
@@ -0,0 +1,11 @@
+# original author: https://gist.github.com/dceoy/99d976a2c01e7f0ba1c813778f9db744
+
+def read_vcf(path):
+    with open(path, 'r') as f:
+        lines = [l for l in f if not l.startswith('##')]
+    return pd.read_csv(
+        io.StringIO(''.join(lines)),
+        dtype={'#CHROM': str, 'POS': int, 'ID': str, 'REF': str, 'ALT': str,
+               'QUAL': str, 'FILTER': str, 'INFO': str},
+        sep='\t'
+    ).rename(columns={'#CHROM': 'CHROM'})
